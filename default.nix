a030ebe5c6b0e4152fe850373d14427bd3b16abd
akmal <isakulovdev@gmail.com>
2026-03-03 13:38:53 +0500

    feat: use data from `Cargo.toml`

--- default.nix
+++ default.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
+let
+  config = pkgs.lib.importTOML ./Cargo.toml;
+  package = config.package;
+in
 pkgs.rustPlatform.buildRustPackage {
-  pname = "sae";
-  version = "0.1";
+  pname = package.name;
+  version = package.version;
 
   src = pkgs.lib.cleanSource ./.;
   cargoLock.lockFile = ./Cargo.lock;
